66e1ae37317a1725619669916aa5d50d2f8c3eda
Akshay <akshay.sharma09695@gmail.com>
2020-05-20 21:44:47 +0530

    Use host from env

--- client/src/SocketServer.js
+++ client/src/SocketServer.js
@@ -8,7 +8,9 @@ let onlinePlayers = [];
 /*================================================
 | Colyseus connection with server
 */
-var client = new Colyseus.Client('ws://localhost:3000');
+
+const url = process.env.SOCKET_HOST || 'localhost:3000';
+var client = new Colyseus.Client(`ws://${url}`);
 let room = client.joinOrCreate("poke_world").then(room => {
     console.log(room.sessionId, "joined", room.name);
     return room
